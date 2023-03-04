6686081d19861063fa84b9c9134538966022f496
wrhsd1 <wangrenhuan@gmail.com>
2023-03-04 19:40:48 +0800

    Update example_story.py

--- example_story.py
+++ example_story.py
@@ -1,12 +1,6 @@
 from story_rewrite import StoryTeller
 
-
-with open('chat_log.txt', 'r') as f:
-                    lines = f.readlines()
-                    last_line = lines[-1]
-                    if last_line:
-                        story_background = last_line
-
+story_background = "你在树林里冒险，指不定会从哪里蹦出来一些奇怪的东西，你握紧手上的手枪，希望这次冒险能够找到一些值钱的东西，你往树林深处走去。"
 
 if __name__ == "__main__":
     chatter = StoryTeller(story_background)
